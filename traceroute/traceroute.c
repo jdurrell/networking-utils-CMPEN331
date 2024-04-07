859808185c4293c46ee86d9682119b64eae51f8b
John Durrell <john.m.durrell@gmail.com>
2024-04-07 15:10:29 -0400

    Clean up traceroute output.

--- traceroute/traceroute.c
+++ traceroute/traceroute.c
@@ -22,8 +22,10 @@ struct SendPacket {
     uint8_t payload[8];
 };
 
-struct TimeExceededResponse {
+struct Response {
     struct iphdr iphdr;
+    // ICMP message format is the same for both 'Time Exceeded' and 'Port Unreachable' Messages.
+    // User must check the type and code fields to know which.
     struct {
         struct icmphdr icmphdr;
         struct iphdr originalIpHdr;
@@ -32,7 +34,7 @@ struct TimeExceededResponse {
 };
 
 // Hardcoded constants for proof-of-concept.
-uint32_t GOOGLE_IP_ADDRESS = (8 << 24) + (8 << 16) + (8 << 8) + (8);
+uint32_t DEST_IP = (8 << 24) + (8 << 16) + (8 << 8) + (8);
 uint32_t MAGIC = (13 << 24) + (0 << 16) + (94 << 8) + (35);
 uint16_t SOURCE_PORT = 3000;
 uint16_t DEST_PORT = 32768 + 666;
@@ -67,6 +69,23 @@ void debugPrintICMPInfo(struct icmphdr hdr) {
     printf("Checksum: %u\n", hdr.checksum);
 }
 
+uint64_t totalMicroseconds(struct timeval time) {
+    return (((uint64_t)(time.tv_sec)) * 1000000) + (uint64_t)(time.tv_usec);
+}
+
+void printOutputLine(int ttl, uint32_t addr, struct timeval send, struct timeval recv) {
+    uint64_t latency = totalMicroseconds(recv) - totalMicroseconds(send);
+    printf("%d: %d.%d.%d.%d (%lu.%lums)\n",
+        ttl,
+        (ntohl(addr) & 0xFF000000) >> 24,
+        (ntohl(addr) & 0x00FF0000) >> 16,
+        (ntohl(addr) & 0x0000FF00) >> 8,
+        (ntohl(addr) & 0x000000FF),
+        latency / 1000,
+        latency % 1000
+    );
+}
+
 // Calculate the 16-bit checksum of the given byte array. RFC 1071.
 uint16_t checksum(uint8_t* arr, int len) {
     uint64_t checksum = 0;
@@ -96,7 +115,7 @@ struct SendPacket buildMessage() {
     message.udphdr.source = htons(SOURCE_PORT);
     message.udphdr.dest = htons(DEST_PORT);
     message.udphdr.len = htons(sizeof(message));
-    message.udphdr.check = 0;  // I *think* this have the destination ignore the checksum.
+    message.udphdr.check = 0;  // Tells the destination to ignore the checksum.
 
     // Build payload.
     for (int i = 0; i < sizeof(message.payload); i++) {
@@ -112,7 +131,7 @@ int main() {
 
     // Set up address struct.
     struct sockaddr_in destAddr;
-    destAddr.sin_addr.s_addr = GOOGLE_IP_ADDRESS;
+    destAddr.sin_addr.s_addr = DEST_IP;
 
     // Create socket for sending UDP messages.
     int sendSockId = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
@@ -121,6 +140,7 @@ int main() {
         return 1;
     }
 
+    // Set up receiver socket for ICP messages.
     int recvSockId = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (recvSockId == -1) {
         printf("Error creating receiver socket.\n");
@@ -133,53 +153,61 @@ int main() {
     }
 
     struct SendPacket message = buildMessage();
-    // uint8_t msgbuffer[8];
-    // memset(msgbuffer, 0, sizeof(msgbuffer));
-    // memcpy(msgbuffer, &MAGIC, sizeof(MAGIC));
 
-    int ttl = 2;
+    int ttl = 1;
     uint32_t raddr = 0;
+    struct timeval sendTime;
+    struct timeval recvTime;
     uint8_t buffer[RECV_BUFFER_SIZE];
-    while ((raddr != GOOGLE_IP_ADDRESS) && (ttl < 30)) {        
+    struct Response* response;
+    response = (struct Response*) buffer;
+
+    while ((raddr != DEST_IP) && (ttl < 30)) {        
+        // Reset receiving structures.
         memset(buffer, 0, RECV_BUFFER_SIZE);
+
+        // Set TTL for this iteration.
         error = setsockopt(sendSockId, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
         if (error < 0) {
             printf("Failed to set send socket option: %d\n", errno);
         }
 
         int bytes = sendto(sendSockId, (void*)(&message), sizeof(message), 0, (struct sockaddr*)(&destAddr), sizeof(destAddr));
+        gettimeofday(&sendTime, NULL);
         if (bytes < 0) {
             printf("Error sending message.\n");
             return 1;
         }
-        printf("\nSent message with ttl %d\n", ttl);
-        ttl++;
 
-        struct sockaddr_in returnAddr;
-        uint32_t returnAddrSize = sizeof(returnAddr);
-        bytes = recvfrom(recvSockId, buffer, RECV_BUFFER_SIZE, 0, (struct sockaddr*)(&returnAddr), &(returnAddrSize));
+        bytes = recvfrom(recvSockId, buffer, RECV_BUFFER_SIZE, 0, NULL, NULL);
+        gettimeofday(&recvTime, NULL);
         if ((bytes < 0) && (errno != EAGAIN)) {
             printf("Error receiving message: %d\n", errno);
             return 1;
         }
-        printf("Received %d bytes.\n", bytes);
 
         if ((bytes < 0) && (errno == EAGAIN)) {
-            printf("Timeout reached. No response received.\n");
+            printf("%d: Timeout reached. No response received.\n", ttl);
         } else {
-            struct TimeExceededResponse* response;
-            response = (struct TimeExceededResponse*) buffer;
-            raddr = (uint32_t) response->iphdr.saddr;
-            debugPrintBufferBytes(buffer, bytes);
-            printf("\nMessage Header:\n");
-            debugPrintIpHeader(response->iphdr);
-            printf("\nICMP Header:\n");
-            debugPrintICMPInfo(response->responseBody.icmphdr);
-            printf("\nOriginal message header:\n");
-            debugPrintIpHeader(response->responseBody.originalIpHdr);
-            printf("\nOriginal message bytes:\n");
-            debugPrintBufferBytes((uint8_t*)(&(response->responseBody.originalBytes)), 8);
+            raddr = (uint32_t) (response->iphdr.saddr);
+            printOutputLine(ttl, raddr, sendTime, recvTime);
+            // debugPrintBufferBytes(buffer, bytes);
+            // printf("\nMessage Header:\n");
+            // debugPrintIpHeader(response->iphdr);
+            // printf("\nICMP Header:\n");
+            // debugPrintICMPInfo(response->responseBody.icmphdr);
+            // printf("\nOriginal message header:\n");
+            // debugPrintIpHeader(response->responseBody.originalIpHdr);
+            // printf("\nOriginal message bytes:\n");
+            // debugPrintBufferBytes((uint8_t*)(&(response->responseBody.originalBytes)), 8);
         }
+
+        // Increment ttl to discover next hop.
+        ttl++;
+    }
+
+    if (raddr != DEST_IP) {
+        printf("Max TTL exceeded. Did not receive response from destination.\n");
     }
 
     return 0;
